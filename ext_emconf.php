3c316c294eb5d84f0e74aa2f42293ff86a5ad9ee
Andreas Nedbal <andreas.nedbal@in2code.de>
2022-05-05 17:24:44 +0200

    [TASK] Raise supported TYPO3 version to 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' => 
     array (
       'caretaker_instance' => '1.0.0-2.99.99',
-      'typo3' => '7.6.0-8.7.99',
+      'typo3' => '7.6.0-9.5.99',
     ),
     'conflicts' => 
     array (
