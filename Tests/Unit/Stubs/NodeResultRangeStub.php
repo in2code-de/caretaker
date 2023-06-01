944bd852ff3823cab25597432fff9a3d076f9dc4
Andreas Nedbal <andreas.nedbal@in2code.de>
2023-06-01 17:17:25 +0200

    [TASK] Remove (almost) everything

--- Tests/Unit/Stubs/NodeResultRangeStub.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Caretaker\Caretaker\Tests\Unit\Stubs;
-
-class NodeResultRangeStub extends \tx_caretaker_NodeResultRange
-{
-}
--- ext_emconf.php
+++ ext_emconf.php
@@ -7,37 +7,20 @@ $EM_CONF[$_EXTKEY] = array (
   'author_email' => 'ficzel@work.de,hempel@work.de,hlubek@networkteam.com,typo3@etobi.de',
   'state' => 'stable',
   'uploadfolder' => 0,
-  'createDirs' => 'typo3temp/caretaker/charts',
   'clearCacheOnLoad' => 0,
   'author_company' => '',
   'version' => '1.0.2',
-  'constraints' => 
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
-      'caretaker_instance' => '1.0.0-3.99.99',
       'typo3' => '7.6.0-12.4.99',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
-  'autoload' => 
-  array (
-    'classmap' => 
-    array (
-      0 => 'Classes',
-      1 => 'interfaces',
-      2 => 'scheduler',
-      3 => 'pi_abstract',
-      4 => 'pi_base',
-      5 => 'pi_graphreport',
-      6 => 'pi_overview',
-      7 => 'pi_singleview',
-    ),
-  ),
-  '_md5_values_when_last_written' => '',
 );
